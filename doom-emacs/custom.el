935374c4ce286a25f2099ebc8b1224412857ba91
weland <weland@bupt.edu.cn>
2021-12-13 16:22:53 +0800

    merge theme

--- doom-emacs/custom.el
+++ doom-emacs/custom.el
@@ -4,43 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-<<<<<<< HEAD
-   ["#2E3440" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
- '(custom-safe-themes
-   '("4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" default))
- '(exwm-floating-border-color "#1c2028")
- '(fci-rule-color "#4C566A")
- '(highlight-tail-colors ((("#394147") . 0) (("#37424e") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#191C25" "#81A1C1"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#191C25" "#A3BE8C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#191C25" "#434C5E"))
- '(objed-cursor-color "#BF616A")
- '(package-selected-packages '(rime))
- '(pdf-view-midnight-colors (cons "#ECEFF4" "#2E3440"))
- '(rustic-ansi-faces
-   ["#2E3440" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
- '(vc-annotate-background "#2E3440")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#A3BE8C")
-    (cons 40 "#bbc28b")
-    (cons 60 "#d3c68b")
-    (cons 80 "#EBCB8B")
-    (cons 100 "#e2b482")
-    (cons 120 "#d99d79")
-    (cons 140 "#D08770")
-    (cons 160 "#c68984")
-    (cons 180 "#bd8b98")
-    (cons 200 "#B48EAD")
-    (cons 220 "#b77f96")
-    (cons 240 "#bb7080")
-    (cons 260 "#BF616A")
-    (cons 280 "#a05b67")
-    (cons 300 "#815664")
-    (cons 320 "#625161")
-    (cons 340 "#4C566A")
-    (cons 360 "#4C566A")))
-=======
    ["#1b1d1e" "#d02b61" "#60aa00" "#d08928" "#6c9ef8" "#b77fdb" "#00aa80" "#dddddd"])
  '(custom-safe-themes
    '("f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" "613aedadd3b9e2554f39afe760708fc3285bf594f6447822dd29f947f0775d6c" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" default))
@@ -76,7 +39,6 @@
     (cons 320 "#704654")
     (cons 340 "#505050")
     (cons 360 "#505050")))
->>>>>>> 5ad7771f2cc3161160767e7e790b3014dbefd312
  '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
