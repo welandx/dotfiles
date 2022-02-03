cc7c2d8a7dfc07f06e92d1bdc16ab5d05c2600fd
weland <weland@bupt.edu.cn>
2022-02-03 21:21:08 +0800

    doom emacs

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -53,19 +53,151 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+;;;;input
+;;(use-package rime
+;;  :init
+;;  (setq rime-disable-predicates
+;;        '(;;rime-predicate-punctuation-line-begin-p       ;; 行首的符号
+;;          rime-predicate-current-input-punctuation-p    ;; 所有的符号
+;;          rime-predicate-space-after-cc-p               ;; 中文空格之后
+;;          rime-predicate-tex-math-or-command-p          ;; tex
+;;          rime-predicate-current-uppercase-letter-p     ;; 大写字符
+;;          ;;rime-predicate-punctuation-after-space-cc-p   ;; 中文空格之后的符号
+;;          rime-predicate-after-ascii-char-p             ;; 英文之后
+;;          ))
+;;  :custom
+;;  (default-input-method "rime"))
+;;
+;;
+;;;;lsp: clangd
+;;(setq lsp-clients-clangd-args '("-j=3"
+;;                                "--background-index"
+;;                                "--clang-tidy"
+;;                                "--completion-style=detailed"
+;;                                "--header-insertion=never"
+;;                                "--header-insertion-decorators=0"))
+;;(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+;;
+;;
+;;;;APP
+;;;;telega
+;;(use-package! telega
+;;  :bind ("C-c t" . #'telega)
+;;  :init (setq telega-proxies
+;;	      '((:server "127.0.0.1"
+;;			 :port "9981"
+;;                         :enable t
+;;                         :type (:@type "proxyTypeHttp")))
+;;              telega-chat-show-avatars nil)
+;;  (setq telega-chat-fill-column 65)
+;;  (setq telega-emoji-use-images t)
+;;  ;;(setq telega-server-libs-prefix "/nix/store/8n54mnnizmzvi7b3bhv31fc3hr0fcl9i-tdlib-1.7.0/lib")
+;;  :config
+;;  (set-fontset-font t 'unicode (font-spec :family "Symbola") nil 'prepend)
+;;  (with-eval-after-load 'company (add-hook 'telega-chat-mode-hook (lambda ()
+;;                                                                    (make-local-variable 'company-backends)
+;;                                                                    (dolist (it '(telega-company-botcmd telega-company-emoji))
+;;                                                                      (push it company-backends)))))
+;;  (with-eval-after-load 'all-the-icons (add-to-list 'all-the-icons-mode-icon-alist '(telega-root-mode all-the-icons-fileicon "telegram"
+;;                                                                                                      :heigt 1.0
+;;                                                                                                      :v-adjust -0.2
+;;                                                                                                      :face all-the-icons-yellow))
+;;                        (add-to-list 'all-the-icons-mode-icon-alist '(telega-chat-mode all-the-icons-fileicon "telegram"
+;;                                                                                       :heigt 1.0
+;;                                                                                       :v-adjust -0.2
+;;                                                                                       :face all-the-icons-blue)))
+;;  (telega-notifications-mode t)
+;;  (telega-mode-line-mode 1)
+;;  (add-hook 'telega-chat-mode-hook
+;;            (lambda ()
+;;              (toggle-truncate-lines +1)
+;;              (display-line-numbers-mode -1)))
+;;  (add-hook 'telega-root-mode-hook
+;;            (lambda ()
+;;              (toggle-truncate-lines +1)
+;;              (display-line-numbers-mode -1)
+;;              (toggle-truncate-lines -1)))
+;;  (define-key telega-msg-button-map "k" nil))
+;;
+;;;;eaf
+;;(use-package! eaf
+;;  :load-path "~/.emacs.d/site-lisp/emacs-application-framework") ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+;;
+;;(use-package! eaf-browser)
+;;(use-package! eaf-pdf-viewer)
+;;
+
 (use-package rime
-  :init
-  (setq rime-disable-predicates
-        '(;;rime-predicate-punctuation-line-begin-p       ;; 行首的符号
-          rime-predicate-current-input-punctuation-p    ;; 所有的符号
-          rime-predicate-space-after-cc-p               ;; 中文空格之后
-          rime-predicate-tex-math-or-command-p          ;; tex
-          rime-predicate-current-uppercase-letter-p     ;; 大写字符
-          ;;rime-predicate-punctuation-after-space-cc-p   ;; 中文空格之后的符号
-          rime-predicate-after-ascii-char-p             ;; 英文之后
-          ))
   :custom
-  (default-input-method "rime"))
+  (default-input-method "rime")
+  :config
+  (setq rime-show-candidate 'posframe)
+  (setq rime-disable-predicates
+        '(rime-predicate-after-alphabet-char-p
+          rime-predicate-current-input-punctuation-p
+          rime-predicate-space-after-cc-p
+          rime-predicate-current-uppercase-letter-p
+          rime-predicate-tex-math-or-command-p)))
+
+;;(use-package xenops
+;; :hook (org-mode . xenops-mode))
+
+(use-package org-roam
+  :config
+  (setq org-roam-directory (file-truename "~/org")))
+
+(use-package keyfreq
+  :init
+  (keyfreq-mode 1)
+  (keyfreq-autosave-mode 1)
+  :config
+  (setq keyfreq-excluded-commands
+      '(self-insert-command
+        evil-next-line
+        evil-previous-line
+        mwheel-scroll
+        evil-forward-char
+        evil-backward-char)))
+
+(use-package eaf
+  :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+  :custom
+  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-enable-adblocker t)
+  (browse-url-browser-function 'eaf-open-browser))
+  ;;:config
+  ;;(defalias 'browse-web #'eaf-open-browser))
+  ;;(eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  ;;(eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding))
+(use-package eaf-pdf-viewer)
+(use-package eaf-browser)
+(use-package eaf-demo)
+(use-package eaf-rss-reader)
+(use-package eaf-org-previewer)
+(use-package eaf-evil)
+(use-package eaf-org)
+
+(define-key key-translation-map (kbd "SPC")
+    (lambda (prompt)
+      (if (derived-mode-p 'eaf-mode)
+          (pcase eaf--buffer-app-name
+            ("browser" (if  (string= (eaf-call-sync "call_function" eaf--buffer-id "is_focus") "True")
+                           (kbd "SPC")
+                         (kbd eaf-evil-leader-key)))
+            ("pdf-viewer" (kbd eaf-evil-leader-key))
+            ("image-viewer" (kbd eaf-evil-leader-key))
+            (_  (kbd "SPC")))
+        (kbd "SPC"))))
+
+(use-package xenops
+  :config
+  (setq xenops-math-image-scale-factor 2.0))
+
+;;(add-hook 'org-mode-hook #'xenops-mode)
+(add-hook 'latex-mode-hook #'xenops-mode)
+(add-hook 'Latex-mode-hook #'xenops-mode)
+
 (setq lsp-clients-clangd-args '("-j=3"
                                 "--background-index"
                                 "--clang-tidy"
@@ -73,47 +205,16 @@
                                 "--header-insertion=never"
                                 "--header-insertion-decorators=0"))
 (after! lsp-clangd (set-lsp-priority! 'clangd 2))
+(set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))
 
-(use-package! telega
-  :bind ("C-c t" . #'telega)
-  :init (setq telega-proxies
-	      '((:server "127.0.0.1"
-			 :port "9981"
-                         :enable t
-                         :type (:@type "proxyTypeHttp")))
-              telega-chat-show-avatars nil)
-  (setq telega-chat-fill-column 65)
-  (setq telega-emoji-use-images t)
-  ;;(setq telega-server-libs-prefix "/nix/store/8n54mnnizmzvi7b3bhv31fc3hr0fcl9i-tdlib-1.7.0/lib")
+(setq org-hide-emphasis-markers t)
+;;(use-package! dirvish)
+;;(setq default-frame-alist '(alpha-background . 80))
+
+(use-package telega
+  :commands (telega)
+  :defer t
   :config
-  (set-fontset-font t 'unicode (font-spec :family "Symbola") nil 'prepend)
-  (with-eval-after-load 'company (add-hook 'telega-chat-mode-hook (lambda ()
-                                                                    (make-local-variable 'company-backends)
-                                                                    (dolist (it '(telega-company-botcmd telega-company-emoji))
-                                                                      (push it company-backends)))))
-  (with-eval-after-load 'all-the-icons (add-to-list 'all-the-icons-mode-icon-alist '(telega-root-mode all-the-icons-fileicon "telegram"
-                                                                                                      :heigt 1.0
-                                                                                                      :v-adjust -0.2
-                                                                                                      :face all-the-icons-yellow))
-                        (add-to-list 'all-the-icons-mode-icon-alist '(telega-chat-mode all-the-icons-fileicon "telegram"
-                                                                                       :heigt 1.0
-                                                                                       :v-adjust -0.2
-                                                                                       :face all-the-icons-blue)))
+  (setq telega-server-libs-prefix "/usr")
   (telega-notifications-mode t)
-  (telega-mode-line-mode 1)
-  (add-hook 'telega-chat-mode-hook
-            (lambda ()
-              (toggle-truncate-lines +1)
-              (display-line-numbers-mode -1)))
-  (add-hook 'telega-root-mode-hook
-            (lambda ()
-              (toggle-truncate-lines +1)
-              (display-line-numbers-mode -1)
-              (toggle-truncate-lines -1)))
-  (define-key telega-msg-button-map "k" nil))
-(use-package! eaf
-  :load-path "~/.emacs.d/site-lisp/emacs-application-framework") ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-
-(use-package! eaf-browser)
-(use-package! eaf-pdf-viewer)
-
+  (telega-mode-line-mode 1))
